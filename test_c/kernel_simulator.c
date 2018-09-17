57f6cdbd1e8e256c27e5469cb0b1802e2b653983
alekras <krasnop@bellsouth.net>
2018-09-17 15:19:14 -0400

    changes for 2 devices.

---
+++ test_c/kernel_simulator.c
@@ -0,0 +1,66 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "sha256.h"
+
+FILE *fdebug;
+
+char amoveo_update_gpu(BYTE *nonce, BYTE *data);
+void amoveo_stop_gpu();
+void amoveo_gpu_alloc_mem(int, int, int);
+void amoveo_gpu_free_mem();
+void amoveo_mine_gpu(BYTE nonce[23],
+                     WORD difficulty,
+                     BYTE data[32],
+                     WORD GDIM,
+                     WORD BDIM,
+                     WORD id);
+void gpu_info(int device);
+
+int count = 0;
+
+char amoveo_update_gpu(BYTE *nonce, BYTE *data) {
+  fprintf(fdebug,"---- Update: data and nonce \n");
+  fflush(fdebug);
+  count++;
+  if ((count % 9) == 0) {
+	  return (char)1;
+  } else {
+	  return (char)0;
+  }
+}
+
+void amoveo_mine_gpu(BYTE nonce[23],
+                     WORD difficulty,
+                     BYTE data[32],
+                     WORD GDIM,
+                     WORD BDIM,
+                     WORD id) {
+  fprintf(fdebug,"---- amoveo_mine_gpu/6.\n");
+  fflush(fdebug);
+}
+
+void amoveo_stop_gpu() {
+  fprintf(fdebug,"---- amoveo_stop_gpu/0.\n");
+  fflush(fdebug);
+
+}
+
+void amoveo_gpu_alloc_mem(int device, int gdim, int bdim) {
+  fprintf(fdebug,"---- amoveo_gpu_alloc_mem/3.\n");
+  fflush(fdebug);
+}
+
+void amoveo_gpu_free_mem() {
+  fprintf(fdebug,"---- amoveo_gpu_free_mem/0.\n");
+  fflush(fdebug);
+}
+
+void gpu_info(int device) {
+  fprintf(fdebug,"---- gpu_info/1.\n");
+  fflush(fdebug);
+}
+
